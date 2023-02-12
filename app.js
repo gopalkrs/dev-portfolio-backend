54ff042e836729872efba8ae2ae0229253d4c9e1
Gopal Choudhary <raunakchd1@gmail.com>
2023-02-12 23:08:03 +0530

    cors added

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const api = require("./server/api");
 const app = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.use("/api",api);
 app.use(express.static(path.join(__dirname, 'build')));
 
